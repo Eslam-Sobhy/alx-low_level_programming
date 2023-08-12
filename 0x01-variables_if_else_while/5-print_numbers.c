5a7caa633282328af81850bf07b72809fdf97de3
Eslam-Sobhy <badr59964@gmail.com>
2023-08-12 08:54:35 -0700

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A C progrsm prints form 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+	printf("%i", a);
+}
+printf("\n");
+return (0);
+}
